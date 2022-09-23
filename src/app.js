a4fb5f854fceac253936990cc59e216b08805b5b
helyukk <nightcallhellena@gmail.com>
2022-09-23 23:07:18 +0300

    added the search engine

--- src/app.js
+++ src/app.js
@@ -48,8 +48,21 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search (city) {
 let apiKey = "ec78611d825d9f65f95eedc0e996f06f";
-let city = "Kyiv";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Kyiv");
